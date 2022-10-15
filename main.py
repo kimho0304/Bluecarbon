a751fabf076711fd61d5006a95831ac4cad36421
kimho0304 <105331004+kimho0304@users.noreply.github.com>
2022-10-16 02:35:51 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import pyqtSlot
+
+#form_class = uic.loadUiType("index.ui")[0]
+
+#class Form(QMainWindow, form_class):
+class Form(QMainWindow):
+    def __init__(self) :
+        super().__init__()
+        self.ui = uic.loadUi("index.ui", self)
+        #self.setupUi(self)
+        self.ui.show()
+
+    @pyqtSlot()
+    def select(self):
+        self.ui.lineEdit.setText("Test")
+
+    @pyqtSlot()
+    def validate(self):
+        sys.exit()
+
+
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+
+    myWindow = Form()
+
+    myWindow.show()
+
+    app.exec_()
