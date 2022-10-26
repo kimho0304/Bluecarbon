1c49d3be7da118accf7c87faa8823b73c7411f05
kimho0304 <mfkimho0304@naver.com>
2022-10-27 05:01:24 +0900

    Add Graph Window

--- main.py
+++ main.py
@@ -4,9 +4,55 @@ from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
 import pandas as pd
 import numpy as np
-from functools import WRAPPER_ASSIGNMENTS, reduce
 import os
 from PyQt5.QtWidgets import QApplication
+import pyqtgraph as pg
+
+class MyWidget(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        pg.setConfigOptions(background='w')  # 배경 흰색으로.. ; global configuration options
+
+        y = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+        y2 = [0, 1, 2, 4, 12, 14, 16, 17, 14, 22]
+
+        x = range(0, 10)
+
+        pw = pg.PlotWidget()
+
+        pw.showGrid(x=True, y=True)
+
+        pw.addLegend(
+        size=(100, 10))  # LegendItem() 없으면, 생성후 반환 ; 매개변수는 LegendItem() 에서 사용 ==> size=(width,height)
+
+        pw.setLabel("left", text="Value", units="v")  # PlotItem() 메소드
+
+        pw.setLabel('bottom', text="Time", units='s')
+
+        pw.setXRange(0, 10)  # 내부적으로 ViewBox() 메소드 사용함. -- setXRange(min, max, padding=None, update=True)
+
+        pw.setYRange(0, 25)
+
+        pw.plot(x, y, pen='b', symbol='x', symbolPen='g', symbolBrush=0.2, name='green')
+
+        pw.plot(x, y2, pen='r', symbol='o', symbolPen='b', symbolBrush=0.2, name='blue')
+
+        layout = QHBoxLayout()
+
+        layout.addWidget(pw)
+
+        self.setLayout(layout)
+
+        self.setGeometry(300, 100, 550, 650)  # x, y, width, height
+
+        self.setWindowTitle("pyqtgraph 예제 2")
+
+        self.show()
+
+
 
 class Form(QMainWindow):
     def __init__(self) :
@@ -18,6 +64,13 @@ class Form(QMainWindow):
     @pyqtSlot()
     def tmh_new_file(self):
         pass
+
+    @pyqtSlot()
+    def tmh_show_graph(self):
+        myGraph = MyWidget()
+
+        myGraph.show()
+
 
     @pyqtSlot()
     def tmh_open_file(self):
@@ -103,13 +156,8 @@ class Form(QMainWindow):
             
             self.tmh_standard_box_result.setRowCount(len(excel_data.index)-1)
             self.tmh_standard_box_outline.setRowCount(1)
-            self.tmh_standard_box_outline.setItem(0, 0, QTableWidgetItem('0'))
-            self.tmh_standard_box_outline.setItem(0, 1, QTableWidgetItem('0'))
-            self.tmh_standard_box_outline.setItem(0, 2, QTableWidgetItem('0'))
-            self.tmh_standard_box_outline.setItem(0, 3, QTableWidgetItem('0'))
-            self.tmh_standard_box_outline.setItem(0, 4, QTableWidgetItem('0'))
-            self.tmh_standard_box_outline.setItem(0, 5, QTableWidgetItem('0'))
-            self.tmh_standard_box_outline.setItem(0, 6, QTableWidgetItem('0'))
+            for i in range(7):
+                self.tmh_standard_box_outline.setItem(0, i, QTableWidgetItem('0'))
 
             for j in range(0, len(excel_data.index)-1, 1):
                 try:
