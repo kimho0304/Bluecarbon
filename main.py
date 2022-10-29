11523105d6970f941cab4578d596646cd3a131b0
kimho0304 <mfkimho0304@naver.com>
2022-10-29 17:33:36 +0900

    Add Graph Window

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 from PyQt5.QtWidgets import QApplication
 import pyqtgraph as pg
 
-class MyWidget(QWidget):
+class TheGraph(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -50,39 +50,38 @@ class MyWidget(QWidget):
 
         self.setWindowTitle("pyqtgraph 예제 2")
 
-        self.show()
-
-
-
 class Form(QMainWindow):
-    def __init__(self) :
+    def __init__(self):
         super().__init__()
         self.ui = uic.loadUi("originUI.ui", self)
         self.ui.show()
 
-    #Menu(파일)
+    def tmh_show_graph(self):
+        result_graph.show()
+
+    # Menu(파일)
     @pyqtSlot()
     def tmh_new_file(self):
+        self.tmh_inverse_SW_result.setRowCount(0)
+        self.tmh_SW_result.setRowCount(0)
+        self.tmh_standard_box_result.setRowCount(0)
+        self.tmh_standard_box_outline.setRowCount(0)
+        self.tmh_validate_box_result.setRowCount(0)
+        self.tmh_validate_box_input.setText('')
+        self.tmh_standard_box_Input.setText('')
         pass
-
-    @pyqtSlot()
-    def tmh_show_graph(self):
-        myGraph = MyWidget()
-
-        myGraph.show()
-
 
     @pyqtSlot()
     def tmh_open_file(self):
         global fpath
         try:
-            fpath = QFileDialog.getOpenFileName(self, '파일 선택', workdir, filter='Excel file(*xls *xlsx)')    
+            fpath = QFileDialog.getOpenFileName(self, '파일 선택', workdir, filter='Excel file(*xls *xlsx)')
         except NameError:
             fpath = QFileDialog.getOpenFileName(self, '파일 선택', filter='Excel file(*xls *xlsx)')
-        
+
         if fpath[0] != '':
-            global excel_data3
-            excel_data3 = pd.read_excel(fpath[0], sheet_name=2)
+            global excel_data2
+            excel_data2 = pd.read_excel(fpath[0], sheet_name=1)
 
             self.tmh_validate_box_input.setText(fpath[0])
 
@@ -94,22 +93,22 @@ class Form(QMainWindow):
 
     @pyqtSlot()
     def tmh_save_this(self):
-        global excel_data3
-        excel_data3.to_excel(fpath[0].replace('.xls','_result.xls'), index=False)
-        QMessageBox.information(self,'Success!','저장이 완료되었습니다.')
+        global excel_data2
+        excel_data2.to_excel(fpath[0].replace('.xls', '_result.xls'), index=False)
+        QMessageBox.information(self, 'Success!', '저장이 완료되었습니다.')
         pass
 
     @pyqtSlot()
     def tmh_custom_save(self):
-        global excel_data3
+        global excel_data2
         try:
-            savepath = QFileDialog.getSaveFileName(self, '파일 저장', workdir, filter='Excel file(*xls *xlsx)')    
+            savepath = QFileDialog.getSaveFileName(self, '파일 저장', workdir, filter='Excel file(*xls *xlsx)')
         except NameError:
             savepath = QFileDialog.getSaveFileName(self, '파일 저장', filter='Excel file(*xls *xlsx)')
         if savepath[0] != '':
-            excel_data3.to_excel(savepath[0].replace('.xlsx', '').replace('.xls', '')+'.xlsx', index=False)
-            QMessageBox.information(self,'Success!','저장이 완료되었습니다.')
-        
+            excel_data2.to_excel(savepath[0].replace('.xlsx', '').replace('.xls', '') + '.xlsx', index=False)
+            QMessageBox.information(self, 'Success!', '저장이 완료되었습니다.')
+
         pass
 
     @pyqtSlot()
@@ -122,7 +121,7 @@ class Form(QMainWindow):
             self.tmh_validate_box_result.removeRow(0)
         pass
 
-    #Menu(선택)
+    # Menu(선택)
     @pyqtSlot()
     def tmh_set_workdir(self):
         global workdir
@@ -133,18 +132,20 @@ class Form(QMainWindow):
     def tmh_set_type_code(self):
         global standard_path
         try:
-            standard_path = QFileDialog.getOpenFileName(self, '파일 선택', workdir, filter='Excel file(*xls *xlsx)')    
+            standard_path = QFileDialog.getOpenFileName(self, '파일 선택', workdir, filter='Excel file(*xls *xlsx)')
         except NameError:
             standard_path = QFileDialog.getOpenFileName(self, '파일 선택', filter='Excel file(*xls *xlsx)')
-        
+
         if standard_path[0] != '':
             self.tmh_standard_box_Input.setText(standard_path[0])
 
             standard_data = pd.read_excel(standard_path[0], sheet_name=0)
-            num0 = num1 = num2 = num3 = num4 = num5 = tot = 0 
-            
+            num0 = num1 = num2 = num3 = num4 = num5 = tot = 0
+
             global excel_data
-            
+
+            global ambi_list
+            ambi_list = list()
             standard_list = list()
             base_list = list()
 
@@ -153,27 +154,31 @@ class Form(QMainWindow):
 
             for i in range(1, len(excel_data.index), 1):
                 base_list.append(excel_data['종명자료'][i])
-            
-            self.tmh_standard_box_result.setRowCount(len(excel_data.index)-1)
+
+            self.tmh_standard_box_result.setRowCount(len(excel_data.index) - 1)
             self.tmh_standard_box_outline.setRowCount(1)
+
             for i in range(7):
                 self.tmh_standard_box_outline.setItem(0, i, QTableWidgetItem('0'))
 
-            for j in range(0, len(excel_data.index)-1, 1):
+            for j in range(0, len(excel_data.index) - 1, 1):
                 try:
-                    standard = standard_data['Group'][standard_list.index(excel_data['종명자료'][j+1])]
+                    standard = standard_data['Group'][standard_list.index(excel_data['종명자료'][j + 1])]
                     self.tmh_standard_box_result.setItem(j, 0, QTableWidgetItem(str(base_list[j])))
+                    if standard == 0:
+                        standard = 2
                     self.tmh_standard_box_result.setItem(j, 1, QTableWidgetItem(str(standard)))
                 except ValueError:
-                    standard = 2 #2로 고정
+                    standard = 2  # 2로 고정
                     self.tmh_standard_box_result.setItem(j, 0, QTableWidgetItem(str(base_list[j])))
                     self.tmh_standard_box_result.setItem(j, 1, QTableWidgetItem(str(standard)))
                     self.tmh_standard_box_result.setItem(j, 2, QTableWidgetItem('신규'))
 
                 excel_data['Group'][j] = standard
-                
+
                 if standard == 0:
-                    num0 += 1
+                    excel_data['Group'][j] = 2
+                    num2 += 1  # 미할당은 2로 교체
                     self.tmh_standard_box_outline.setItem(0, 6, QTableWidgetItem(str(num0)))
                 elif standard == 1:
                     num1 += 1
@@ -190,23 +195,25 @@ class Form(QMainWindow):
                 elif standard == 5:
                     num5 += 1
                     self.tmh_standard_box_outline.setItem(0, 5, QTableWidgetItem(str(num5)))
-            
+
+                ambi_list.append(standard)
+
                 tot += 1
                 self.tmh_standard_box_outline.setItem(0, 0, QTableWidgetItem(str(tot)))
-                
+
                 QApplication.processEvents()
-            
+
         pass
 
-    #Menu(도움말)
+    # Menu(도움말)
     @pyqtSlot()
     def tmh_open_help(self):
-        os.system('"'+os.getcwd()+'/help.html"')
+        os.system('"' + os.getcwd() + '/help.html"')
         pass
 
     @pyqtSlot()
     def tmh_open_info(self):
-        QMessageBox.about(self,'이 도구에 관하여','About Message')
+        QMessageBox.about(self, '이 도구에 관하여', 'About Message')
         pass
 
     @pyqtSlot()
@@ -217,8 +224,6 @@ class Form(QMainWindow):
             excel_data = excel_data.fillna(0)
 
             global excel_data2
-            excel_data2 = pd.read_excel(fpath[0], sheet_name=1)
-
             jj_count = len(excel_data2.index)
 
             self.tmh_validate_box_result.setRowCount(jj_count)
@@ -226,33 +231,103 @@ class Form(QMainWindow):
             num = 1
             while num <= jj_count:
                 if num / 1000 >= 1:
-                    N_num = 'N'+str(num)
+                    N_num = 'N' + str(num)
                 elif num / 100 >= 1:
-                    N_num = 'N0'+str(num)
+                    N_num = 'N0' + str(num)
                 elif num / 10 >= 1:
-                    N_num = 'N00'+str(num)
+                    N_num = 'N00' + str(num)
                 else:
-                    N_num = 'N000'+str(num)
-                
-                self.tmh_validate_box_result.setItem(num-1, 0, QTableWidgetItem(N_num))
-                self.tmh_validate_box_result.setItem(num-1, 1, QTableWidgetItem('이상없음'))
-            
+                    N_num = 'N000' + str(num)
+
+                self.tmh_validate_box_result.setItem(num - 1, 0, QTableWidgetItem(N_num))
+                self.tmh_validate_box_result.setItem(num - 1, 1, QTableWidgetItem('정상'))
+
                 num += 1
-                
+
                 QApplication.processEvents()
 
         except NameError:
-            QMessageBox.information(self,'Error!','검증할 파일을 선택하지 않았습니다.')
+            QMessageBox.information(self, 'Error!', '검증할 파일을 선택하지 않았습니다.')
         pass
 
-    #종의 정보 출력 영역
+    # 종의 정보 출력 영역
     @pyqtSlot()
     def tmh_change_type_code(self):
         pass
 
-    #산정할 지수 선택
+    # 산정할 지수 선택
     @pyqtSlot()
     def tmh_calc_ambi(self):
+        global excel_data
+        global excel_data2
+
+        jj_count = len(excel_data2.index)
+
+        self.tmh_AZTI_result.setRowCount(jj_count)
+
+        global AMBI_list
+        AMBI_list = list()
+
+        num = 1
+
+        while num <= jj_count:
+            N = 0
+
+            if num / 1000 >= 1:
+                N_num = 'N' + str(num)
+            elif num / 100 >= 1:
+                N_num = 'N0' + str(num)
+            elif num / 10 >= 1:
+                N_num = 'N00' + str(num)
+            else:
+                N_num = 'N000' + str(num)
+
+            self.tmh_AZTI_result.setItem(num - 1, 0, QTableWidgetItem(N_num))
+
+            for i in range(2, len(excel_data.index), 1):
+                N += excel_data[N_num][i]
+
+            global ambi_list
+            pG1 = pG2 = pG3 = pG4 = pG5 = 0
+            sG1 = sG2 = sG3 = sG4 = sG5 = AMBI = 0.0
+
+            for i in range(2, len(excel_data.index), 1):
+                # AMBI
+                if ambi_list[i - 2] == 1:
+                    pG1 += excel_data[N_num][i]
+                elif ambi_list[i - 2] == 2:
+                    pG2 += excel_data[N_num][i]
+                elif ambi_list[i - 2] == 3:
+                    pG3 += excel_data[N_num][i]
+                elif ambi_list[i - 2] == 4:
+                    pG4 += excel_data[N_num][i]
+                elif ambi_list[i - 2] == 5:
+                    pG5 += excel_data[N_num][i]
+
+            sG1 = pG1 / N * 100
+            sG2 = pG2 / N * 100
+            sG3 = pG3 / N * 100
+            sG4 = pG4 / N * 100
+            sG5 = pG4 / N * 100
+
+            AMBI = ((0 * sG1) + (1.5 * sG2) + (3 * sG3) + (4.5 * sG4) + (6 * sG5)) / 100
+
+            AMBI_list.append(AMBI)
+            self.tmh_AZTI_result.setItem(num - 1, 1, QTableWidgetItem(str(AMBI)))
+
+            num += 1
+            status = 0
+
+            if (status != int(num / jj_count * 100)):
+                status = int(num / jj_count * 100)
+                self.tmh_AZTI_bar.setValue(status)
+
+            QApplication.processEvents()
+
+        excel_data2['AMBI'] = ''
+        for i in range(0, jj_count, 1):
+            excel_data2['AMBI'][i] = AMBI_list[i]
+
         pass
 
     @pyqtSlot()
@@ -272,57 +347,55 @@ class Form(QMainWindow):
             B = 0
 
             if num / 1000 >= 1:
-                N_num = 'N'+str(num)
+                N_num = 'N' + str(num)
             elif num / 100 >= 1:
-                N_num = 'N0'+str(num)
+                N_num = 'N0' + str(num)
             elif num / 10 >= 1:
-                N_num = 'N00'+str(num)
+                N_num = 'N00' + str(num)
             else:
-                N_num = 'N000'+str(num)
+                N_num = 'N000' + str(num)
 
-            self.tmh_inverse_SW_result.setItem(num-1, 0, QTableWidgetItem(N_num))
+            self.tmh_inverse_SW_result.setItem(num - 1, 0, QTableWidgetItem(N_num))
 
             for i in range(2, len(excel_data.index), 1):
                 N += excel_data[N_num][i]
-                B += excel_data[N_num+'.1'][i]
+                B += excel_data[N_num + '.1'][i]
 
             SEP_up = 0.0
             SEP_dn = 0.0
 
             for i in range(2, len(excel_data.index), 1):
-                #ISEP 등급
+                # ISEP 등급
                 Ni = excel_data[N_num][i]
-                Bi = excel_data[N_num+'.1'][i]
+                Bi = excel_data[N_num + '.1'][i]
                 Pi = Ni / N
                 Pi_ = Bi / B
 
-                SEP_up += Pi_ * np.log(Pi_, where = Pi_ > 0)
-                SEP_dn += Pi * np.log(Pi, where = Pi > 0)
+                SEP_up += Pi_ * np.log(Pi_, where=Pi_ > 0)
+                SEP_dn += Pi * np.log(Pi, where=Pi > 0)
 
                 if SEP_dn == 0.0:
                     ISEP = 0.0
                 else:
                     SEP = -SEP_up / -SEP_dn
                     ISEP_n = (1 / SEP) + 1
-                    ISEP = np.log10(ISEP_n, where = ISEP_n > 0)
-		
+                    ISEP = np.log10(ISEP_n, where=ISEP_n > 0)
+
             ISEP_list.append(ISEP)
-            self.tmh_inverse_SW_result.setItem(num-1, 1, QTableWidgetItem(str(ISEP)))
+            self.tmh_inverse_SW_result.setItem(num - 1, 1, QTableWidgetItem(str(ISEP)))
 
             num += 1
             status = 0
-            
-            if(status != int(num/jj_count*100)):
-                status = int(num/jj_count*100)
+
+            if (status != int(num / jj_count * 100)):
+                status = int(num / jj_count * 100)
                 self.tmh_inverse_SW_bar.setValue(status)
-            
+
             QApplication.processEvents()
 
-
-        global excel_data3
-        excel_data3['ISEP'] = ''
+        excel_data2['ISEP'] = ''
         for i in range(0, jj_count, 1):
-            excel_data3['ISEP'][i] = ISEP_list[i]
+            excel_data2['ISEP'][i] = ISEP_list[i]
 
         pass
 
@@ -345,19 +418,19 @@ class Form(QMainWindow):
             B = 0
 
             if num / 1000 >= 1:
-                N_num = 'N'+str(num)
+                N_num = 'N' + str(num)
             elif num / 100 >= 1:
-                N_num = 'N0'+str(num)
+                N_num = 'N0' + str(num)
             elif num / 10 >= 1:
-                N_num = 'N00'+str(num)
+                N_num = 'N00' + str(num)
             else:
-                N_num = 'N000'+str(num)
+                N_num = 'N000' + str(num)
 
-            self.tmh_SW_result.setItem(num-1, 0, QTableWidgetItem(N_num))
+            self.tmh_SW_result.setItem(num - 1, 0, QTableWidgetItem(N_num))
 
             for i in range(2, len(excel_data.index), 1):
                 N += excel_data[N_num][i]
-                B += excel_data[N_num+'.1'][i]
+                B += excel_data[N_num + '.1'][i]
 
             sw_ln = 0.0
             sw_log = 0.0
@@ -366,41 +439,40 @@ class Form(QMainWindow):
                 Ni = excel_data[N_num][i]
                 Pi = Ni / N
 
-                #Shannon-Wiener Index
-                sw_ln -= Pi * np.log(Pi, where = Pi > 0)
-                sw_log -= Pi * np.log2(Pi, where = Pi > 0)
+                # Shannon-Wiener Index
+                sw_ln -= Pi * np.log(Pi, where=Pi > 0)
+                sw_log -= Pi * np.log2(Pi, where=Pi > 0)
 
             sw_ln_list.append(sw_ln)
-            self.tmh_SW_result.setItem(num-1, 1, QTableWidgetItem(str(sw_ln)))
-            
+            self.tmh_SW_result.setItem(num - 1, 1, QTableWidgetItem(str(sw_ln)))
+
             sw_log_list.append(sw_log)
-            self.tmh_SW_result.setItem(num-1, 2, QTableWidgetItem(str(sw_log)))
+            self.tmh_SW_result.setItem(num - 1, 2, QTableWidgetItem(str(sw_log)))
 
             num += 1
             status = 0
-            
-            if(status != int(num/jj_count*100)):
-                status = int(num/jj_count*100)
+
+            if (status != int(num / jj_count * 100)):
+                status = int(num / jj_count * 100)
                 self.tmh_SW_bar.setValue(status)
-            
+
             QApplication.processEvents()
 
-        global excel_data3
-        excel_data3['Shannon-Wiener Index (ln)'] = ''
+        excel_data2['Shannon-Wiener Index (ln)'] = ''
         for i in range(0, jj_count, 1):
-            excel_data3['Shannon-Wiener Index (ln)'][i] = sw_ln_list[i]
+            excel_data2['Shannon-Wiener Index (ln)'][i] = sw_ln_list[i]
 
-        excel_data3['Shannon-Wiener Index (log2)'] = ''
+        excel_data2['Shannon-Wiener Index (log2)'] = ''
         for i in range(0, jj_count, 1):
-            excel_data3['Shannon-Wiener Index (log2)'][i] = sw_log_list[i]
-            
+            excel_data2['Shannon-Wiener Index (log2)'][i] = sw_log_list[i]
+
         pass
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-
     myWindow = Form()
-
     myWindow.show()
 
+    result_graph = TheGraph()
     app.exec_()
